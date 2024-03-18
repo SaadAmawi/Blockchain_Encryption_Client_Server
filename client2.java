d8d9c233c88cfe99a22c00431d22d9ed9fac53fc
Saad <saadamawi@yahoo.com>
2024-03-18 21:15:36 +0400

    added 2 more clients to send requests at the same time

--- client2.java
+++ client2.java
@@ -1,9 +1,13 @@
-import java.net.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
-public class client{
-
-    public static void main(String [] args){
+public class client2 {
+      public static void main(String [] args){
         
         DatagramSocket aSocket = null;
         File outputFile = new File("C:\\Users\\USER\\Desktop\\DS ASS 1\\outputClient1.txt");
@@ -15,11 +19,12 @@ public class client{
             InetAddress serverIP = InetAddress.getByName(args[0]);
             int port = 20000;
             FileWriter fileWriter = new FileWriter(outputFile, true); // FileWriter object
+
             // byte [] msg = args[1].getBytes();
 
             for(int i =1;i<=30;i++){
                 long startTime = System.nanoTime();
-                String mes = new String(i+","+"This is client 1 sending message: "+i);
+                String mes = new String(i+","+"This is client 2 sending message: "+i);
                 DatagramPacket req = new DatagramPacket(mes.getBytes(),mes.length(),serverIP, port);
                 aSocket.send(req);
                 
@@ -38,8 +43,7 @@ public class client{
                     e.printStackTrace();
                 }
             }
-            System.out.println(new String(resp.getData(), 0, resp.getLength()));
-        }
+            System.out.println(new String(resp.getData(),0,resp.getLength()));}
 
         }catch(SocketException e){}catch(IOException e){}finally{
             if(aSocket != null){
@@ -50,5 +54,4 @@ public class client{
 
 
     }
-
 }
